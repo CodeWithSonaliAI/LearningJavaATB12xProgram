400d43ab7a558b97f6c92168cde2d58592caec3e
Sonali <Sonali041091@gmail.com>
2025-07-05 22:30:29 +0530

    more tasks added

---
+++ src/WhileLoop18thJune/Lab118_While.java
@@ -0,0 +1,10 @@
+package WhileLoop18thJune;
+
+public class Lab118_While {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello, Aishwarya");
+        }
+    }
+}
+//Infinite time it will run bcz condition is always true
---
+++ src/WhileLoop18thJune/Lab120_While_Check.java
@@ -0,0 +1,12 @@
+package WhileLoop18thJune;
+
+public class Lab120_While_Check {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 0){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+// Output is ntg bcz condition false
